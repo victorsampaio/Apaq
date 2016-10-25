1dd6acc1510c26f339432ca046d45c05ec1ae52d
Victor Sampaio <prolinsprojects@gmail.com>
2016-10-25 01:58:07 -0300

    Included FABRIC #10
    Included Fabric - #10 : Crashlytics

--- app/src/main/java/com/br/apaq/SplashScreenActivity.java
+++ app/src/main/java/com/br/apaq/SplashScreenActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashScreenActivity extends AppCompatActivity implements Runnable {
 
@@ -12,6 +15,7 @@ public class SplashScreenActivity extends AppCompatActivity implements Runnable 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.activity_splash_screen);
 
         Handler handler = new Handler();
